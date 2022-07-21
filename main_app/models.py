162ae4f524553009997b39bbef22ad0d469d6d97
mileslathan <mlathan.cn@gmail.com>
2022-07-21 13:32:15 -0400

    forgot to commit multiple times. Now starting with create, delete and edit functions of finch/vinyl collector

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    label = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    released = models.IntegerField()
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'record_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
-
+from .models import Record
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
-
-class Record:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, label, style, released, country):
-    self.name = name
-    self.label = label
-    self.style = style
-    self.released = released
-    self.country = country
-
-records = [
-  Record('PHD & MC Conrad Progression Sessions / 3 By 4', 'Looking Good Records', 'Intelligent D&B', '1997', 'UK'),
-  Record('Smith & Pledger - Believe', 'Anjuna', 'Trance', '2004', 'UK'),
-  Record('Unknown Artist - UTIQUE 002', 'UTIQUE', 'Deep House', '2016', 'Germany')
-]
 
 def home(request):
     return HttpResponse('<h1>The home route is working :3</h1>')
@@ -23,4 +10,22 @@ def about(request):
     return render(request, 'about.html')
 
 def records_index(request):
+    records = Record.objects.all()
     return render(request, 'records/index.html', { 'records': records })
+
+def records_detail(request, record_id):
+    record = Record.objects.get(id=record_id)
+    return render(request, 'records/detail.html', { 'record': record })
+
+class RecordCreate(CreateView):
+  model = Record
+  fields = '__all__'
+  success_url = '/records/'
+
+class RecordUpdate(UpdateView):
+    model = Record
+    fields = ['label', 'style', 'released', 'country']
+
+class RecordDelete(DeleteView):
+    model = Record
+    success_url = '/records/'
