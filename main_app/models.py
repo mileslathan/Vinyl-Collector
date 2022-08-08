8e129c859d3ea1fe691f6e5a5f506484d511625d
Miles Lathan <mlathan.cn@gmail.com>
2022-08-08 16:21:39 -0400

    committing to push.

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Record(models.Model):
@@ -8,6 +9,7 @@ class Record(models.Model):
     style = models.CharField(max_length=100)
     released = models.IntegerField()
     country = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
